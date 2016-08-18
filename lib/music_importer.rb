99f3d81d6041fe75b10e402fec24f7ef33e8bf7f
andela-bkanyolo <bernard.kanyolo@andela.com>
2016-08-18 10:10:29 +0300

    (Feature) Implement reading of mp3 files from path [Issue #15]

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -2,6 +2,14 @@ class MusicImporter
   def initialize(path)
     @path = path
   end
-  
+
   attr_reader :path
+
+  def files
+    Dir.entries(@path).select { |dir| dir.chomp.end_with?(".mp3")}
+  end
+
+  def import
+
+  end
 end
