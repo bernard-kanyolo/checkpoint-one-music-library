b80fe44a9bfe6716d1e5d8ca20ce47206cee0ef3
andela-bkanyolo <bernard.kanyolo@andela.com>
2016-08-21 23:54:21 +0300

    (Chore) Refractor song class to style guide specs

--- lib/song.rb
+++ lib/song.rb
@@ -9,24 +9,18 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    @artist = artist
-    if artist
-      artist.add_song(self)
-    end
-    @genre = genre
-    if genre
-      genre.songs << self unless genre.songs.include?(self)
-    end
+    self.artist = artist
+    self.genre = genre
   end
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self)
+    artist && artist.add_song(self)
   end
 
   def genre=(genre)
     @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
+    genre && (genre.songs << self unless genre.songs.include?(self))
   end
 
   def self.all
@@ -48,12 +42,12 @@ class Song
 
   def self.new_from_filename(filename)
     artist, name, genre = filename.chomp('.mp3').split(' - ').collect(&:strip)
-
-    Song.new(name, Artist.find_or_create_by_name(artist), Genre.find_or_create_by_name(genre))
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(genre)
+    Song.new(name, artist, genre)
   end
 
   def self.create_from_filename(filename)
     Song.new_from_filename(filename).save
   end
-
 end
