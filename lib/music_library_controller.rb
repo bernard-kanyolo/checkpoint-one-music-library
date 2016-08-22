2e531780fdc2bf4da30964c28d5983d1e6ecfff1
andela-bkanyolo <bernard.kanyolo@andela.com>
2016-08-22 23:48:24 +0300

    (Chore) Refractor View related methods out of the controller

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,75 +1,40 @@
 class MusicLibraryController
-  def initialize(path = './db/mp3s')
+  def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
+    @view = MusicLibraryView.new
   end
 
   def call
-    puts "Welcome!"
-    get_input
+    @view.home
+    input
   end
 
-  def get_input
-   while true
-     print "Command me > "
-     command = gets.strip.chomp.downcase
+  def input
+    while true
+      @view.prompt
+      command = gets.chomp.strip.downcase
 
-     case command
-     when 'exit' then break
-     when 'list songs' then list_songs
-     when 'list artists' then list_artists
-     when 'list genres' then list_genres
-     when 'play song' then play_song
-     when 'list artist' then list_artist
-     when 'list genre' then list_genre
-     else puts "No such command available"
-     end
-   end
-  end
-
-  def list_songs
-    Song.all.each_with_index { |song, index|
-      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
-  end
-
-  def list_artists
-    Artist.all.each{ |artist| puts artist.name }
-  end
-
-  def list_genres
-    Genre.all.each{ |genre| puts genre.name }
-  end
-
-  def play_song
-    print "Which song would you like to play? "
-    num = gets.strip.chomp.to_i
-    if (1..Song.all.length).include?(num)
-      song = Song.all[num - 1]
-      puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    else
-      puts "Please enter a valid song number between 1 and #{Song.all.length}"
+      case command
+      when 'exit'
+        @view.exit
+        break
+      when 'list songs' then @view.list_songs
+      when 'list artists' then @view.list_artists
+      when 'list genres' then @view.list_genres
+      when 'play song'
+        @view.prompt("play_song")
+        num = gets.chomp.strip.to_i
+        @view.play_song(num)
+      when 'list artist'
+        @view.prompt("list_artist")
+        artist = gets.chomp.strip
+        @view.list_artist(artist)
+      when 'list genre'
+        @view.prompt("list_genre")
+        genre = gets.chomp.strip
+        @view.list_genre(genre)
+      else @view.default
+      end
     end
   end
-
-  def list_artist
-    print "Which artist's songs would you like to view? "
-    artist = Artist.find_by_name(gets.strip.chomp)
-    if artist
-      artist.songs.each { |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
-    else
-      puts "No such artist exists in the music library"
-    end
-  end
-
-  def list_genre
-    print "Which genre's songs would you like to view? "
-    genre = Genre.find_by_name(gets.strip.chomp)
-    if genre
-      genre.songs.each { |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"}
-    else
-      puts "No such genre exists in the music library"
-    end
-  end
-  
 end
